dd2e28d94f3e77cc85b0adac0522dbc5eadfa5d8
GITPractice <nehetechandan25@gmail.com>
2025-06-11 17:19:38 +0530

    changes made

--- src/controllers/regCtrl.js
+++ src/controllers/regCtrl.js
@@ -1,51 +1,72 @@
 const bcrypt = require("bcryptjs");
+let RegService = require("../services/regService.js");
 const regmodel = require("../models/regModel.js");
+//const { acceptregdata } = require("../services/regService.js");
 exports.regCtrl = (req, res) => {
   res.render("nav.ejs"); // This loads views/nav.ejs
 };
 
 exports.signUpPage = (req, res) => {
-  res.render("register.ejs", { message: "" });
+  res.render("register.ejs", { msg: "" });
 };
 
 exports.signInPage = (req, res) => {
-  res.render("login.ejs", { message: "" });
+  res.render("login.ejs", { msg: "" });
 };
 exports.registerUser = (req, res) => {
-  const { name, email, contact, username, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  const userData = {
+  const { name, email, contact, oldusername, newusername, password } = req.body;
+  //const hashedPassword = bcrypt.hashSync(password, 10);
+  let result = RegService.acceptregdata(
     name,
     email,
     contact,
-    username,
-    password: hashedPassword,
-  };
-
-  regmodel.insertUser(userData)
-    .then(() => {
-      res.render("login.ejs", { message: "Registration successful. Please log in." });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.render("register.ejs", { message: "Registration failed." });
-    });
+    oldusername,
+    newusername,
+    password
+  );
+  res.render("register.ejs", { msg: result });
 };
+
+//   const userData = {
+//     name,
+//     email,
+//     contact,
+//     username,
+//     password: hashedPassword,
+//   };
+
+//   regmodel.insertUser(userData)
+//     .then(() => {
+//       res.render("login.ejs", { message: "Registration successful. Please log in." });
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.render("register.ejs", { message: "Registration failed." });
+//     });
+// };
 
 exports.validateUser = (req, res) => {
-  const { username, password } = req.body;
-  regmodel.validateUserFromDB(username)
-    .then((result) => {
-      if (result.length > 0 && bcrypt.compareSync(password, result[0].password)) {
-        req.session.uid = result[0].id;
-        res.render("nav.ejs");
-      } else {
-        res.render("login.ejs", { message: "Invalid username or password" });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.render("error.ejs", { error: err });
-    });
-};
+    const { username, password } = req.body;
+  
+    regmodel.validateUserFromDB(username)
+      .then((result) => {
+        if (result.length === 0) {
+          return res.render("login.ejs", { message: "User not found. Please register first." });
+        }
+  
+        const user = result[0];
+        const match = bcrypt.compareSync(password, user.password); // compare hash
+  
+        if (match) {
+          req.session.uid = user.id;
+          res.render("nav.ejs");
+        } else {
+          res.render("login.ejs", { message: "Incorrect password." });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.render("error.ejs", { error: err });
+      });
+  };
+  
--- src/models/regModel.js
+++ src/models/regModel.js
@@ -1,11 +1,26 @@
-const db = require("../config/db"); // your MySQL connection
+let conn = require("../config/db.js"); // your MySQL connection
 
-exports.insertUser = (userData) => {
-  const { name, email, contact, username, password } = userData;
+// exports.insertUser = (userData) => {
+//   const { name, email, contact, username, password } = userData;
 
+//   return new Promise((resolve, reject) => {
+//     const sql = "INSERT INTO user (name, email, contact, username, password) VALUES (?, ?, ?, ?, ?)";
+//     db.query(sql, [name, email, contact, username, password], (err, result) => {
+//       if (err) reject(err);
+//       else resolve(result);
+//     });
+//   });
+// };
+
+exports.saveuser = (...regdata) => {
+  conn.query("insert into user values('0',?,?,?,?,?)", [...regdata]);
+  return true;
+};
+
+exports.validateUserFromDB = (username, password) => {
   return new Promise((resolve, reject) => {
-    const sql = "INSERT INTO user (name, email, contact, username, password) VALUES (?, ?, ?, ?, ?)";
-    db.query(sql, [name, email, contact, username, password], (err, result) => {
+    const sql = "SELECT * FROM user WHERE username = ? and password = ?";
+    conn.query(sql, [username, password], (err, result) => {
       if (err) reject(err);
       else resolve(result);
     });
